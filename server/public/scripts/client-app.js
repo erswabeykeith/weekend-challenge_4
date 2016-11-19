2823af079feef9ee71624c10eb9c964e5aa2919b
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-18 19:01:13 -0600

    task test worked

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-console.log('test');
+  console.log('test');
+  // getTasks();
+
+  // add a task
+  $('#task-submit').on('click', postTask);
+
+
+
 });
+
+//Functions
+function getTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/tasks',
+    success: function(tasks) {
+      appendTasks(tasks);
+    },
+    error: function() {
+      console.log('Database error');
+    }
+  })
+}
+
+function appendTasks() {
+  $("#container").empty();
+  for (var i = 0; i < tasks.length; i++) {
+    // $("#container").append('<div class="row book"></div>');
+    $el = $('#container').children().last();
+    var task = tasks[i];
+    $el.data('id', task.id);
+    $el.append(task);
+  }
+}
+
+function postTask() {
+  console.log("postTask test");
+}
